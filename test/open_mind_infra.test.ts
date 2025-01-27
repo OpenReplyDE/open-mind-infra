702a7584a842b419afc0b82cdd4eb9114eec934a
Armin Jazi <a.ghajarjazi@reply.de>
2025-01-27 16:22:01 +0100

    Initial commit

---
+++ test/open_mind_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpenMindInfra from '../lib/open_mind_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/open_mind_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpenMindInfra.OpenMindInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
